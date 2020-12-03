bd068953380396c554c6fca1909ca80d06720f8f
cmlozanos <carlosmlozanos@gmail.com>
2020-12-03 17:55:50 +0100

    add junit 5 add first step

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ProductionTest {
+
+    @Test
+    void caseTrue(){
+        Assertions.assertTrue(true);
+    }
 }
