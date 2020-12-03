c8b742f4e632cadaab000554aa6e846ccfa3101c
cmlozanos <carlosmlozanos@gmail.com>
2020-12-03 18:20:14 +0100

    test case 1 to 100 elements

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ProductionTest {
 
     @Test
-    void caseTrue(){
-        Assertions.assertTrue(true);
+    void caseListOfNumber1To100(){
+        Assertions.assertEquals(IntStream.range(0, 100).boxed().collect(Collectors.toList()), result());
+    }
+
+    private List<Integer> result() {
+        return IntStream.range(0, 100).boxed().collect(Collectors.toList());
     }
 }
