a2653290bf78197b521373e126e74ef8df8560f8
cmlozanos <carlosmlozanos@gmail.com>
2020-12-03 19:09:33 +0100

    add Buzz case

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,18 +11,21 @@ import java.util.stream.IntStream;
 class ProductionTest {
 
     @Test
-    void caseListOfString1To100WithFizzAsModuleThreeNumbers() {
-        Assertions.assertEquals(IntStream.range(0, 100).boxed().map(this::applyFizzBuzzLogic).collect(Collectors.toList()),
-                result());
+    void caseListOfString1To5WithFizzAsModuleThreeNumbersAndBuzzFiveNumber() {
+        Assertions.assertEquals(Arrays.asList("1", "2", "Fizz", "4", "Buzz"),result().stream().limit(5).collect(Collectors.toList()));
+        Assertions.assertEquals(100,result().size());
     }
 
     private List<String> result() {
-        return IntStream.range(0, 100).boxed().map(this::applyFizzBuzzLogic).collect(Collectors.toList());
+        return IntStream.rangeClosed(1, 100).boxed().map(this::applyFizzBuzzLogic).collect(Collectors.toList());
     }
 
     private String applyFizzBuzzLogic(Integer e) {
         if (e % 3 == 0) {
             return "Fizz";
+        }
+        if (e % 5 == 0) {
+            return "Buzz";
         }
         return e.toString();
     }
@@ -29,6 +33,11 @@ class ProductionTest {
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12, 18, 21})
     void caseModuleThreeNumberIsFizz(int moduleThreeNumber) {
-        Assertions.assertEquals("Fizz", result().get(moduleThreeNumber));
+        Assertions.assertEquals("Fizz", result().get(moduleThreeNumber - 1));
+    }
+
+    @Test
+    void caseModuleFiveNumberIsBuzz(){
+        Assertions.assertEquals("Buzz", result().get(4));
     }
 }
